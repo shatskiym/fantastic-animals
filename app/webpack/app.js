814aaa549d41ed80e4cf00a4202672a740ab10d6
shatskiym <michael.shatskiy@gmail.com>
2017-06-27 20:16:30 +0300

    React from npm

--- app/webpack/app.js
+++ app/webpack/app.js
@@ -1,3 +1,8 @@
-require('./src/cart.js')
-require('./src/new.js')
-require('./src/new2.js')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Board from './src/components/terrains.jsx'
+
+document.addEventListener("DOMContentLoaded", function(){
+  var terrains = document.getElementById('board_react_container').getAttribute('data');
+  ReactDOM.render(<Board data = {JSON.parse(terrains)}/>, document.getElementById('board_react_container'));
+});
